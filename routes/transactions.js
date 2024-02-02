0d41988bc0fd297e1166d3c7cbbf0ba2a689192c
justina177 <justinaenenche@yahoo.com>
2024-02-02 16:22:47 +0100

    Added post reauest route

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,12 @@
+const { addIncome } = require('../controllers/income.js')
+
 const router = require('express').Router()
 
 
 router.get('/', (req, res) => {
     res.send('Hello world')
 })
+
+router.post('/add-income', addIncome)
 
 module.exports = router
