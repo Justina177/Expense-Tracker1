aa257997ee6ba36e94ed4d4e959b2812972d0936
justina177 <justinaenenche@yahoo.com>
2024-02-02 07:36:35 +0100

    Added get route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const db = require('./db/db.js')
+const {readdirSync} = require('fs')
+
 const app = express();
 
 const dotenv = require("dotenv").config()
@@ -10,6 +12,9 @@ const PORT = process.env.PORT || 5000
 // middlewares
 app.use(express.json())
 app.use(cors())
+
+// routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
