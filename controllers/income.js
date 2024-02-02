5a0d716db24e4410d0b5c7a97f6724eaf202bcd6
justina177 <justinaenenche@yahoo.com>
2024-02-02 17:47:39 +0100

    Made correction in the getIncomes controller

--- controllers/income.js
+++ controllers/income.js
@@ -35,7 +35,7 @@ exports.addIncome = async (req, res) => {
 exports.getIncomes = async (req, res) => {
     try {
         const incomes = await IncomeSchema.find().sort({createdAt: -1})
-        res.status(500).json(incomes)
+        res.status(200).json(incomes)
     } catch (err) {
         res.status(500).json({message: 'Server Error'})
     }
@@ -44,7 +44,6 @@ exports.getIncomes = async (req, res) => {
 
 exports.deleteIncome = async (req, res) => {
     const {id} = req.params;
-    console.log(params)
     IncomeSchema.findByIdAndDelete(id)
     .then((income) => {
         res.status(200).json({message: 'Income deleted'})
